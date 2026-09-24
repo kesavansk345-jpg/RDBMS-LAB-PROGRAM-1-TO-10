d937d5b94273da7e8e1407f767bde12a5a7fc155
kesavansk345-jpg <kesavansk345@gmail.com>
2026-09-24 12:51:17 +0530

    Update solution.sql

--- Assignment10_Left_Right_Join/solution.sql
+++ Assignment10_Left_Right_Join/solution.sql
@@ -1,13 +1,7 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
-
--- Create Course
-
--- Create Enrollment
-
--- Insert sample records
-
--- LEFT JOIN
-
--- RIGHT JOIN
+use Ajay;
+create table Cource(courceID int(10), courcename varchar(20), credits int(5));
+insert into Cource values(201,"Database systems",4),(202,"Data structures",3),(203,"Mathematics",4);
+create table Enrollment(enrollmentID int(5),studentID int(10),courseID int(10));
+insert into Enrollment values(1,1001,201),(2,1001,202),(3,1002,203),(4,1003,201);
+select  Cource.courceID,Cource.courcename,Cource.credits from Cource right join Enrollment on Cource.courceID=Enrollment.courseID;
+Cource.courceID=Enrollment.courseID;
