64abc9c2c76c927b1adb5ad903a7b77429f5fa5f
kesavansk345-jpg <kesavansk345@gmail.com>
2026-09-24 12:41:21 +0530

    Update solution.sql

--- Assignment04_Create_Course/solution.sql
+++ Assignment04_Create_Course/solution.sql
@@ -1,9 +1,7 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
+use collegeDB;
+create table Cource(courceID int(10) primary key,courceName varchar(20),Credits int(10),departmentID int(5));
+insert into Cource values (101,"BCA",4,201),(102,"BSC IT",5,204),(103,"BBA",4,210);
+desc Cource;
 
--- Create Course table
+select * from Cource;
 
--- Insert three records
-
--- Display structure
