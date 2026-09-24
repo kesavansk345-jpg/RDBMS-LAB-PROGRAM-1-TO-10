82464a7bee77e99ef9f0958364c0b28378d5afe9
kesavansk345-jpg <kesavansk345@gmail.com>
2026-09-24 12:49:43 +0530

    Update solution.sql

--- Assignment09_Inner_Join/solution.sql
+++ Assignment09_Inner_Join/solution.sql
@@ -1,12 +1,8 @@
-DROP DATABASE IF EXISTS CollegeDB;
-CREATE DATABASE CollegeDB;
-USE CollegeDB;
+create database Ajay;
+use Ajay;
+create table Department(DepartmentID int(10) PRIMARY KEY,Departmentname varchar(50));
+insert into Department values(101,"Computer Science"),(102,"Mathematics"),(103,"Science");
+create table Student(StudentID int(10) PRIMARY KEY,Studentname varchar(20),DepartmentID int(10));
+insert into Student values(1001,"Arun",101),(1002,"Divya",102),(1003,"Karthik",101),(1004,"Nisha",103);
+Select Student.StudentID, Student.Studentname, Department.Departmentname from Student INNER JOIN Department on Student.DepartmentID=Department.departmentID;
 
-
--- Create Department
-
--- Create Student
-
--- Insert sample records
-
--- INNER JOIN query
